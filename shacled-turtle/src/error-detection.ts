09386b3a8843aebf99fb1551d4347e0715fdaf5e
BruJu <bruyat.ju@live.fr>
2022-04-19 12:23:51 +0200

    Lint should not produce multiple times an error on the same position anymore

--- shacled-turtle/src/error-detection.ts
+++ shacled-turtle/src/error-detection.ts
@@ -2,23 +2,44 @@ import { linter, Diagnostic } from "@codemirror/lint";
 import { EditorView } from "@codemirror/view";
 import { syntaxTree } from "@codemirror/language";
 
+class RangedErrors {
+  knownIntervals: { from: number, to: number }[] = [];
+
+  addError(from: number, to: number) {
+    // addError is called through a tree exploration. Therefore, it is in
+    // practice impossible to have overlapping intervals such as one of
+    // the interval is not included in the second one.
+    for (const existing of this.knownIntervals) {
+      if (existing.from <= from && existing.to >= to) return;
+
+      if (from <= existing.from && to >= existing.to) {
+        existing.from = from;
+        existing.to = to;
+        return;
+      }
+    }
+
+    this.knownIntervals.push({ from, to });
+  }
+}
+
 function lintExample(view: EditorView): readonly Diagnostic[] {
-  const diagnostics: Diagnostic[] = [];
+  const rangedErrors = new RangedErrors();
 
   syntaxTree(view.state).iterate({
     enter: (type, from, to) => {
       if (type.isError) {
-        diagnostics.push({
-          from,
-          to,
-          severity: "error",
-          message: "There is an error around here",
-        });
+        rangedErrors.addError(from, to);
       }
     },
   });
 
-  return diagnostics;
+  return rangedErrors.knownIntervals
+  .map(({ from, to }) => ({
+    from, to,
+    severity: "error",
+    message: "There is a syntax error around here"
+  }));
 }
 
 const lint = linter(view => lintExample(view));
